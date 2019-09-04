c0943319f196e5ca5bc14f67b36dabf91ca886c3
Symeonw <Symeonwhite16@gmail.com>
2019-09-04 10:27:11 -0500

    sql 3.5.1

--- database-exercises/3.5.1_where_exercise.sql.sql
+++ database-exercises/3.5.1_where_exercise.sql.sql
@@ -1,6 +1,6 @@
 use employees; 
 describe employees;
-select * from employees where first_name = 'Irena' or first_name ='Vidya' or first_name = ' Maya' and gender = M;
+select * from employees where (first_name = 'Irena' or first_name ='Vidya' or first_name = ' Maya') and gender = "M";
 select * from employees where last_name like 'E%'or last_name like "%E";
 select * from employees where last_name like 'E%'and last_name like "%E";
 select * from employees where hire_date > "1990" and hire_date < "2000";
