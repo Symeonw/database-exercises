26e92a4dac80191e5496e9fe8b6d7895adbd49b7
Symeonw <Symeonwhite16@gmail.com>
2019-09-04 15:22:11 -0500

    sql update .3.1

--- database-exercises/3.6_functions_exercise.sql
+++ database-exercises/3.6_functions_exercise.sql
@@ -4,7 +4,7 @@ select concat(first_name," ", last_name) as full_name from employees where last_
 
 select upper(concat(first_name," ", last_name)) as full_name from employees where last_name like 'E%'or last_name like "%E";
 
-select datediff(curdate(), hire_date) from employees where birth_date like "%-12-25" and hire_date > "1990" and hire_date < "2000";
+select first_name, last_name, datediff(curdate(), hire_date) as "Tenure_in_days" from employees where birth_date like "%-12-25" and hire_date > "1990" and hire_date < "2000";
 
 select max(salary), min(salary) from salaries;
 use employees;
