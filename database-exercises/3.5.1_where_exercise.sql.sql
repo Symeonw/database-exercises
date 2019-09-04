5618b1ddccc7c0d9b7dac85701a762a496bef8ba
Symeonw <Symeonwhite16@gmail.com>
2019-09-04 10:25:45 -0500

    sql 3.5.1

---
+++ database-exercises/3.5.1_where_exercise.sql.sql
@@ -0,0 +1,8 @@
+use employees; 
+describe employees;
+select * from employees where first_name = 'Irena' or first_name ='Vidya' or first_name = ' Maya' and gender = M;
+select * from employees where last_name like 'E%'or last_name like "%E";
+select * from employees where last_name like 'E%'and last_name like "%E";
+select * from employees where hire_date > "1990" and hire_date < "2000";
+select * from employees where birth_date like "%-12-25" and hire_date > "1990" and hire_date < "2000";
+select * from employees where last_name like '%q%' and last_name not like "%qu%"; 
