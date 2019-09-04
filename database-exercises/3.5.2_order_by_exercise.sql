3c8f714e42c478eea0a8f3092dc0cde021e77c4c
Symeonw <Symeonwhite16@gmail.com>
2019-09-04 11:39:17 -0500

    sql update.1

--- database-exercises/3.5.2_order_by_exercise.sql
+++ database-exercises/3.5.2_order_by_exercise.sql
@@ -7,7 +7,7 @@ select * from employees where last_name like 'E%'or last_name like "%E";
 select * from employees where last_name like 'E%'and last_name like "%E" order by emp_no asc;
 select * from employees where last_name like 'E%'and last_name like "%E" order by emp_no desc;
 select * from employees where hire_date > "1990" and hire_date < "2000";
-select * from employees where birth_date like "%-12-25" and hire_date > "1990" and hire_date < "2000" order by birth_date desc, hire_date desc;
+select * from employees where birth_date like "%-12-25" and hire_date > "1990" and hire_date < "2000" order by birth_date asc, hire_date desc;
 select * from employees where last_name like '%q%' and last_name not like "%qu%"; 
 
 			
