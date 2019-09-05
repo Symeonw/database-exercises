3b6bec75238ff211a801c830b95bdfb991922226
Symeonw <Symeonwhite16@gmail.com>
2019-09-05 14:50:27 -0500

    sql update .6

--- database-exercises/3.8.2_join_exercises.sql
+++ database-exercises/3.8.2_join_exercises.sql
@@ -49,4 +49,60 @@ join dept_manager as manager
 on manager.emp_no = employee.emp_no
 join departments as dept
 on dept.dept_no = manager.dept_no
-where manager.to_date = '9999-01-01'
+where manager.to_date = '9999-01-01';
+
+use employees;
+describe departments;
+describe current_dept_emp;
+describe dept_manager;
+describe employees;
+select  dept.dept_name as "Department Name", concat(employee.first_name, " ", employee.last_name) as "Manager Name" from employees as employee
+join dept_manager as manager
+on manager.emp_no = employee.emp_no
+join departments as dept
+on dept.dept_no = manager.dept_no
+where manager.to_date = '9999-01-01' and employee.gender = 'F';
+
+use employees;
+describe titles;
+describe employees;
+select * from departments;
+
+use employees;
+describe titles;
+describe employees;
+describe dept_emp;
+describe departments;
+
+select t.title as title, count(*) from employees as employee
+join titles as t
+on t.emp_no = employee.emp_no
+join dept_emp as depte
+on depte.emp_no = employee.emp_no
+join departments as dept
+on dept.dept_no = depte.dept_no
+where dept.dept_name = "Customer Service" and t.to_date = '9999-01-01'
+group by title;
+
+describe salaries;
+select * from salaries;
+select  dept.dept_name as "Department Name", concat(employee.first_name, " ", employee.last_name) as "Manager Name", sal.salary from employees as employee
+join dept_manager as manager
+on manager.emp_no = employee.emp_no
+join departments as dept
+on dept.dept_no = manager.dept_no
+join salaries as sal
+on sal.emp_no = employee.emp_no
+where manager.to_date = '9999-01-01' and sal.to_date = '9999-01-01';
+
+describe departments;
+describe dept_emp;
+describe employees;
+
+select dept.dept_no, dept.dept_name as dept_name, count(*) from employees as employee
+join dept_emp as depte
+on depte.emp_no = employee.emp_no
+join departments as dept
+on dept.dept_no = depte.dept_no
+where depte.to_date = '9999-01-01'
+group by dept.dept_no;
