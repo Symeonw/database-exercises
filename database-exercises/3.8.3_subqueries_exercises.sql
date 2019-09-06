c93ceebee7d67b46e2d980d65db647ac261f1c99
Symeonw <Symeonwhite16@gmail.com>
2019-09-06 12:09:48 -0500

    sql update 0.8.1

--- database-exercises/3.8.3_subqueries_exercises.sql
+++ database-exercises/3.8.3_subqueries_exercises.sql
@@ -29,7 +29,13 @@ on sal.emp_no = employees.emp_no
 where employees.emp_no in(select emp_no from salaries where salary > (select avg(salary) from salaries) and to_date = "9999-01-01") and sal.to_date='9999-01-01';
 
 
- select first_name, last_name, sal.salary from employees
+ select count(*),count(*)/select count(*) from salaries where to_date>now())*100 from salaries where to_date>now() and salary >(select max(salary)-stddev(salary) from salaries)
+  from employees
 join salaries as sal
 on sal.emp_no = employees.emp_no
 where employees.emp_no in(select emp_no from salaries where salary > (select max(salary) - STDDEV(salary) from salaries) and to_date = "9999-01-01") and sal.to_date='9999-01-01';
+
+select count(*),count(*)/(select count(*) from salaries where to_date="9999-01-01")*100 
+from salaries where to_date="9999-01-01"and salary >(select max(salary)-stddev(salary) 
+from salaries)
+ 
